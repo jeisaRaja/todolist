3990b733dbed791441781b81eeafb64253dfb724
jeisaraja <jeisa.raja.jrt@gmail.com>
2021-12-27 18:59:41 +0700

    ui on progress

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
-import {Todo} from './todo_class'
-import {Project} from './project_class'
+import {Todo} from './todo_class';
+import {Project} from './project_class';
+import './style.css';
 
-const content = document.querySelector('.content')
+// QUERY SELECTOR
+const content = document.querySelector('.content');
+const h1 = document.querySelector('.title_text');
+const navbar = document.querySelector('.navbar');
 
+const h1_todo = "<h2>Your</h2>Todo List";
+h1.innerHTML = h1_todo;
+
+const nav_main = "<div class='navItem blue leftNav'><h5>Todo's</h5></div><div class='navItem rightNav'><h5>Project's</h5></div>"
+navbar.innerHTML = nav_main;
