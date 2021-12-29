82d76eff5d520e18b38f6839d7922e00ed811dad
jeisaraja <jeisa.raja.jrt@gmail.com>
2021-12-29 17:02:08 +0700

    added simple functionality

--- journal.md
+++ journal.md
@@ -6,3 +6,14 @@ There will be 'Project' class that contains the 'Todo' objects. User can make to
 that Todo will automatically assigned to some default Project object, like 'Default' or 'Unkown' or 'General' or just 'Todo'.
 
 The Project object will contain an array of todos.
+
+## Day 2
+### 29 Dec 2021
+So the plan is to make an array of projects and todos.
+We need to loop the array with forEach and each loop we add new todo div to the content.
+
+Need to find out how the navigation work because now, there is a crucial problem. that is we cant divide the content_body separately.
+We need to loop for the project / todo then we also need to push that into the html.
+
+possible solution : 
+    / make a function that determine which array the app should loop over, if user chose tab todo, then the function need to clear the innerHTML of the content_body and then loop over project array and then append child.
--- src/index.js
+++ src/index.js
@@ -6,9 +6,85 @@ import './style.css';
 const content = document.querySelector('.content');
 const h1 = document.querySelector('.title_text');
 const navbar = document.querySelector('.navbar');
+const content_title = document.querySelector('.content_title');
+const btn = document.querySelector('.click')
+const submit = document.querySelector('.submit')
+const form_project = document.querySelector('.form_project')
+const modal = document.querySelector('.modal')
+const content_body = document.querySelector('.content_body')
+
 
 const h1_todo = "<h2>Your</h2>Todo List";
 h1.innerHTML = h1_todo;
 
-const nav_main = "<div class='navItem blue leftNav'><h5>Todo's</h5></div><div class='navItem rightNav'><h5>Project's</h5></div>"
+const nav_main = "<button class='navItem blue leftNav active' id='todos'>Todo's</button><button class='navItem rightNav' id='projects'>Project's</button>"
 navbar.innerHTML = nav_main;
+const navItem = document.querySelectorAll('.navItem')
+const todos = document.querySelector('#todos')
+const projects = document.querySelector('#projects')
+
+navItem.forEach(nav =>{
+    nav.addEventListener('click', clickListen, false)
+})
+
+let array_project = []
+let array_todo = []
+
+btn.addEventListener('click', ()=>{
+    if(modal.style.display == 'flex'){
+        modal.style.display = 'none'
+    }
+    else{
+        modal.style.display = 'flex'
+    }
+})
+
+document.addEventListener('DOMContentLoaded', function(){
+
+    submit.addEventListener('click', addProject
+        // console.log(form_project[0].value , form_project[1].value)
+    ,false); 
+},false);
+
+function addProject(e){
+    e.preventDefault();
+    const title = form_project[0].value
+    const desc = form_project[1].value
+    const empty_array =[]
+    const project = new Project(title,desc,empty_array)
+    console.log(array_project)
+    array_project.push(project)
+    showProject()
+}
+
+function showProject(){
+    content_body.innerHTML=""
+    array_project.forEach(project=>{
+        const h3 = document.createElement('h3')
+        const desc = document.createElement('p')
+
+        h3.textContent = project.title
+        desc.textContent = project.description
+        content_body.appendChild(h3)
+        content_body.appendChild(desc)
+    })
+}
+
+const todos_content = `<h3>This is your todos</h3>`
+const projects_content =  "<h3>This is your projects</h3>"
+content_title.innerHTML = todos_content
+
+function clickListen(e){
+    const button_path = e.path[0].id
+    if(button_path=="todos"){
+        todos.classList.add('active')
+        projects.classList.remove('active')
+        content_title.innerHTML = todos_content
+    }
+    else if(button_path=="projects"){
+        todos.classList.remove('active')
+        projects.classList.add('active')
+        content_title.innerHTML = projects_content
+    }
+}
+
